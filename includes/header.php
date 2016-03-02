6f0e8d806bb2cc0d57f953770605e4311dfb156f
Terry Kelleher <terry@newforce.us>
2016-03-02 11:33:14 -0500

    Local Files for jquery and jquery-ui

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,8 @@
 		<meta charset="UTF-8">
 		<title>Vet School Simulator</title>
-		<link rel="shortcut icon" href="<?= BROWSER_ADMIN; ?>favicon.ico" />		
+		<link rel="shortcut icon" href="<?= BROWSER_HTML; ?>favicon.ico" />		
 		<link rel="stylesheet" href="<?= BROWSER_CSS; ?>common.css" type="text/css" />
-		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+		<link rel="stylesheet" href="/scripts/jquery-ui/1.11.4/jquery-ui.theme.css">
 		<link rel="stylesheet" href="<?= BROWSER_CSS; ?>controls.css" type="text/css" />
 		<link rel="stylesheet" href="<?= BROWSER_CSS; ?>modal.css" type="text/css" />
 		
@@ -11,8 +11,8 @@
 			require_once(SERVER_INCLUDES."phpDefinesToJs.php");
 		?>
 
-		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-		<script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+		<script type="text/javascript" src="/scripts/jquery/2.2.1/jquery.min.js"></script>
+		<script src="/scripts/jquery-ui/1.11.4/jquery-ui.js"></script>
 		<script type="text/javascript" src="<?= BROWSER_SCRIPTS; ?>menu.js"></script>
 		<script type="text/javascript" src="<?= BROWSER_SCRIPTS; ?>chart.js"></script>
 		<script type="text/javascript" src="<?= BROWSER_SCRIPTS; ?>controls.js"></script>
