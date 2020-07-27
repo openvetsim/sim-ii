358307dc9115b7cf940f152028b6a33d9b816c1c
davidwny <dweiner@twcny.rr.com>
2020-07-27 17:02:00 -0400

    Telesim changes

---
+++ js/telesim.js
@@ -0,0 +1,13 @@
+/*
+sim-ii: Copyright (C) 2019  VetSim, Cornell University College of Veterinary Medicine Ithaca, NY
+
+See gpl.html
+*/
+	
+	// telesim functions
+	var telesim = {
+		init: function() {
+console.log("In telesim");			
+			
+		}
+	}
