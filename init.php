81a570e1379dc0fbc02bf9473596e54d2bc0a460
Vet Sim <dweiner@twcny.rr.com>
2021-12-02 15:29:40 -0500

    Rolled rev to 1.6-M

--- init.php
+++ init.php
@@ -125,7 +125,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>
 	
 	// version
 	define('VERSION_MAJOR', 1);
-	define('VERSION_MINOR', '5-M');
+	define('VERSION_MINOR', '6-M');
 	
 	// mobilized
 //	define('MOBILIZED', FALSE);
