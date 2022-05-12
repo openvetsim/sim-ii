ad5815ba74d8cb2b0fe9bbb7c4210f515712cc99
davidwny <dweiner@twcny.rr.com>
2022-05-12 13:25:45 -0400

    Update init.php
    Updated version minor

--- init.php
+++ init.php
@@ -167,7 +167,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>
 	
 	// version
 	define('VERSION_MAJOR', '1');
-	define('VERSION_MINOR', '8-WVS');
+	define('VERSION_MINOR', '9-WVS');
 	
 	// mobilized
 //	define('MOBILIZED', FALSE);
